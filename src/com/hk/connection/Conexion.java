d7a40531a88cf1f04ac21119c88129d8e4ab92bf
Hedwin <ds.hedwin@gmail.com>
2021-03-30 10:22:53 -0400

    ag conexion y splash

---
+++ src/com/hk/connection/Conexion.java
@@ -0,0 +1,47 @@
+
+package com.hk.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    
+    private static Conexion handler = null;
+    
+    private static String DB_URL = "jdbc:mysql://localhost:3306/bd_sapcad";
+    private static String USER = "root";
+    private static String PASS = "1234";
+    private static Connection cnx = null;
+    
+    private Conexion(){
+        
+    }
+    
+    static{
+        createConnection();
+    }
+    
+    public static Conexion getInstance() {
+        if (handler == null) {
+            handler = new Conexion();
+        }
+        return handler;
+    }
+    
+    private static void createConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection(DB_URL, USER, PASS);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Cant load database", "Database Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+    }
+    
+    public Connection getConnection() {
+        return cnx;
+    }
+}
